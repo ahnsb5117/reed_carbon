7e33669438d1a6bb2b2806468279b32c6657c843
Maxwell VanLandschoot <maxvanlan@reed.edu>
2022-12-10 15:04:09 -0800

    app done

--- app_2/app.R
+++ app_2/app.R
@@ -1,3 +1,4 @@
+# Required libraries
 library(shiny)
 library(shinyalert)
 library(shinydashboard)
@@ -10,7 +11,7 @@ library(tidyverse)
 # Tools
 "%ni%" <- Negate("%in%")
 
-# Data
+# Data loading
 country_carbon <- read.csv("country_carbon.csv") %>%
   filter(TIME == 2020,
          LOCATION %ni% c("EU27_2020", "EU28", "G20", "OECD", "OEU", "WLD")) %>%
@@ -20,21 +21,18 @@ country_carbon <- read.csv("country_carbon.csv") %>%
 
 country_carbon$location <- countrycode(c(country_carbon$LOCATION), origin = 'genc3c', destination = 'country.name')
 
-dat <- tibble(case = c(rep("user", 3), rep("ref", 3)), 
-              metric = c("egg","ham","green","egg","ham","green"), 
-              value = c(1,2,4,5,5,5))
-
 # User Interface
 
+# Sidebar
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-    menuItem("About", icon = icon("info-circle"), tabName = "about"),
-    menuItem("Sources and Disclaimer", icon = icon("question-circle"), tabName = "disclaimer")
+    menuItem("Information", icon = icon("info-circle"), tabName = "information")
     
   )
 )
 
+# Main body of app
 body <- dashboardBody(
   tabItems(
     tabItem(tabName = "dashboard",
@@ -123,7 +121,7 @@ body <- dashboardBody(
                        conditionalPanel(
                          condition = "input.meat == 'no'",
                          sliderInput(inputId = "meat_red",
-                                     label = "Servings of Red Meat Per Week (100g)",
+                                     label = "Servings of Red Meat Per Week (4 oz)",
                                      min = 0,
                                      max = 21,
                                      value = 0)
@@ -131,7 +129,7 @@ body <- dashboardBody(
                        conditionalPanel(
                          condition = "input.meat == 'no'",
                          sliderInput(inputId = "meat_white",
-                                     label = "Servings of White Meat or Fish Per Week (100g)",
+                                     label = "Servings of White Meat or Fish Per Week (4 oz)",
                                      min = 0,
                                      max = 21,
                                      value = 0)
@@ -141,7 +139,7 @@ body <- dashboardBody(
                        conditionalPanel(
                          condition = "input.travel == 'fly'",
                          textInput(inputId = "travel_airport",
-                                   label = "Home airport code (e.g. PDX for Portland Intl. Airport)",
+                                   label = "Home IATA airport code (e.g. PDX for Portland Intl. Airport)",
                                    value = "PDX"
                          )
                        ),
@@ -183,15 +181,6 @@ body <- dashboardBody(
                 column(width = 3,
                        conditionalPanel(
                          condition = "input.home == 0",
-                         radioButtons(inputId = "home_type",
-                                      label = "What type of home do you live in?",
-                                      choices = list("Single-Family" = "single_family", 
-                                                     "Apartment" = "aprt"),
-                                      selected = "single_family"
-                         )
-                       ),
-                       conditionalPanel(
-                         condition = "input.home == 0",
                          sliderInput(inputId = "home_roomates",
                                      label = "How many people live in your home?",
                                      min = 1,
@@ -204,11 +193,37 @@ body <- dashboardBody(
             )
     ),
     
-    tabItem(tabName = "about",
-            h2("About tab content")
-    ),
-    tabItem(tabName = "disclaimer",
-            h2("Sources and Disclaimer tab content")
+    tabItem(tabName = "information",
+            h3(strong("About this App")),
+            p("This app was designed by Sung Bum `Simon` Ahn and Maxwell J.D. VanLandschoot for Professor Noelwah
+              Netusil's Environmental Economics Course (Econ 351) in the fall of 2022. The information provided
+              in this app is intented to be used for exploratory and comparative purposes only. The views and statistics
+              found in this app are not necessarily those of Reed College. For additional context for this project, 
+              please reference our case study using",
+              a("this link.", href = "https://docs.google.com/document/d/1CkfImnBcRCdlvdYorSWIJVUbzw327v4DX60YJJjY9fQ/edit?usp=sharing"),
+              ),
+            h3(strong("Disclaimers/Important Notes")),
+            p("The figures presented in this app necessarily use averages and therefore may not accurately reflect the
+              true climate impact of one's activities. In particular, we made the assumption that the breakdown of an 
+              individual's carbon footprint in Oregon would match that of the average American. Likewise, each of our 
+              variable categories (food, transportation, etc.) do not have the granularity required to present a comprehensive
+              view of an individuals carbon footprint. Another important note is that `on campus` housing has an incredibly
+              large carbon footprint because data can not be parsed for individual students. Therefore, the figure presented
+              is the per capita carbon foot print from", em("all"), "gas and electrical use on campus."),
+            h3(strong("About the Data")),
+            p("The data used in this project were drawn from several sources, a comprehensive list of which can be 
+              found in our",
+              a("case study bibliography.", href = "https://docs.google.com/document/d/1CkfImnBcRCdlvdYorSWIJVUbzw327v4DX60YJJjY9fQ/edit?usp=sharing"),
+              "In addition, the following four web resources were used:"),
+            br(),
+            p("- ", a("EPA greenhouse gas equivalencies calculator", href = "https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references")),
+            br(),
+            p("- ", a("CO2 Everything", href = "https://www.co2everything.com/categories"), "a website listing the carbon impact from an extensive list of sources."),
+            br(),
+            p("- ", a("8billiontrees", href = "https://8billiontrees.com/carbon-offsets-credits/reduce-carbon-footprint/average-footprint-per-person/american/"), "a website that aggregates relevent climate change information."),
+            br(),
+            p("- ", a("University of Michigan carbon footprint factsheet", href = "https://css.umich.edu/publications/factsheets/sustainability-indicators/carbon-footprint-factsheet"), "another aggregation of relevant climate change information.")
+            
     )
   )
 )
@@ -224,27 +239,32 @@ ui <- dashboardPage(skin = "green",
 server <- function(input, output) {
   
   dat1 <- reactive({
+    if(input$home == 1){
+      housing <- 8.20
+    }else{
+      housing <- (0.32 * 9.235)/input$home_roomates
+    }
+    
     tibble(
       Case = c("User", "Oregonian", "American"),
       `Air Travel` = c((zip_distance(97202, input$travel_zip, units = "meters")$distance * 0.001 * 0.000251034585607 * 2
-                        + airport_distance("PDX", input$travel_airport) * 0.000099208 * 2)
+                        + airport_distance("PDX", toupper(input$travel_airport)) * 0.000099208 * 2)
                        * input$travel_freq, 
                        0,
                        0),
-      Food = c(input$meat_red * 0.0155 * 52 
-               + input$meat_white * 0.00182 * 52, 
+      Food = c(input$meat_red * 0.00299 * 52 
+               + input$meat_white * 0.00078 * 52, 
                0.14 * 9.235,
-               0.14 * 18),
+               0.14 * 12.90),
       Transportation = c(1/(input$drive_mpg / input$drive_mi) * 0.00887 * 52,
                          0.28 * 9.235,
-                         0.28 * 18),
-      Housing = c(as.numeric(input$home) * 8.20
-                  + (0.32 * 9.235)/input$home_roomates,
+                         0.28 * 12.90),
+      Housing = c(housing,
                   0.32 * 9.235,
-                  0.32 * 18),
+                  0.32 * 12.90),
       Other = c(0,
                 0.26 * 9.235,
-                0.26 * 18)
+                0.26 * 12.90)
     ) %>%
       pivot_longer(cols = 2:6, names_to = "Metric", values_to = "Value")
   })
@@ -311,11 +331,5 @@ server <- function(input, output) {
   })
   
 }
-
-# sources: 
-# https://www.epa.gov/energy/greenhouse-gases-equivalencies-calculator-calculations-and-references
-# https://www.co2everything.com/categories
-# https://8billiontrees.com/carbon-offsets-credits/reduce-carbon-footprint/average-footprint-per-person/american/ Oregon 9.235 tonnes per person
-# https://css.umich.edu/publications/factsheets/sustainability-indicators/carbon-footprint-factsheet 18 tonnes avg American
 
 shinyApp(ui, server)
